21644114e0f558fb4b39b884441e882f0f26bafd
Momentwithace <augustineezekiel763@gmail.com>
2022-10-14 22:16:05 +0100

    Added new classes to db.json

---
+++ chapter4/dictionay_sample.py
@@ -0,0 +1,5 @@
+sample_dict = {'name': 'Peter', 'age': 40}
+# print(sample_dict)
+print('hello', {sample_dict["name"]}, 'you are', {sample_dict["age"]}, 'of age and should consider marriage')
+
+print(f'Hello', {sample_dict['name']}, 'you are', {sample_dict["age"]}, 'of age and should consider marriage')
